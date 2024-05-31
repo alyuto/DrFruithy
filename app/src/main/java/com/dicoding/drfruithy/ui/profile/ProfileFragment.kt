f54e8cf5434eb2e8fc8a497bbe5bba510b209cd4
alyuto <marcwin208@gmail.com>
2024-06-01 05:33:57 +0700

    add darkmode in ProfileFragment and edit all color to android:textColor="?android:textColorPrimary"

--- app/src/main/java/com/dicoding/drfruithy/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/dicoding/drfruithy/ui/profile/ProfileFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.dicoding.drfruithy.databinding.FragmentProfileBinding
@@ -26,7 +28,23 @@ class ProfileFragment : Fragment() {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-
+        darkMode()
         return root
+    }
+
+    private fun darkMode() {
+        binding.switchTheme
+
+        binding.switchTheme.setOnCheckedChangeListener{ _: CompoundButton?, isChecked: Boolean ->
+            if(isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                binding.switchTheme.isChecked = true
+            }
+            else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                binding.switchTheme.isChecked = false
+            }
+
+        }
     }
 }
