90a54a9376b2135dd32d16bd45d09df531697415
alyuto <marcwin208@gmail.com>
2024-05-31 14:21:00 +0700

    fix a navbar

---
+++ app/src/main/java/com/dicoding/drfruithy/ui/profile/ProfileFragment.kt
@@ -0,0 +1,34 @@
+package com.dicoding.drfruithy.ui.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.dicoding.drfruithy.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        val profileViewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
+
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textProfile
+        profileViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+
+        return root
+    }
+}
---
+++ app/src/main/java/com/dicoding/drfruithy/ui/register/RegisterViewModel.kt
@@ -0,0 +1,4 @@
+package com.dicoding.drfruithy.ui.register
+
+class RegisterViewModel {
+}
