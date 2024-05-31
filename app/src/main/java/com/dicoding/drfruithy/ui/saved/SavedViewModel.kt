40fa1c18103d8d55998e4442660eab2c89cfb5b6
alyuto <marcwin208@gmail.com>
2024-05-31 14:37:15 +0700

    edit text in saved and article fragment

--- app/src/main/java/com/dicoding/drfruithy/ui/saved/SavedViewModel.kt
+++ app/src/main/java/com/dicoding/drfruithy/ui/saved/SavedViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class SavedViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is Saved Fragment"
     }
     val text: LiveData<String> = _text
 }
