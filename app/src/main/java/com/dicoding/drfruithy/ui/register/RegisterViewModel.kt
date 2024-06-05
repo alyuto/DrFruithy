76400d7e71881f98233a272d22dfe8f4422f4b1a
alfiansyahY <88750014+alyuto@users.noreply.github.com>
2024-06-05 16:25:50 +0700

    Delete app directory

--- app/src/main/java/com/dicoding/drfruithy/ui/register/RegisterViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.dicoding.drfruithy.ui.register
-
-import androidx.lifecycle.ViewModel
-import com.dicoding.drfruithy.data.Repository
-
-class RegisterViewModel(private val repository: Repository) : ViewModel(){
-
-}
