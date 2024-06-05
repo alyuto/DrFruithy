9051875e8a76b155e043b3afec442ca203066739
Ade Kurniawan <137606009+Adkurrr@users.noreply.github.com>
2024-06-05 22:41:56 +0700

    Update README.bat

--- Machine Learning/README.bat
+++ Machine Learning/README.bat
@@ -2,5 +2,5 @@
 - Dr.Fruithy is an application to detect diseases in fruit plants by detecting the leaves of the fruit plants. 
 - The model built using Convolutional Neural Network architecture
 - Fruit plants that can be detected by this application are mangoes, apples, tomatoes, grapes, oranges and bananas.
-- The dataset can be accessed [here](https://drive.google.com/drive/folders/1QLIqWsjUWQK5b-aiLiKlr3bqfDmbhzcg)
+- The dataset can be accessed here https://drive.google.com/drive/folders/1QLIqWsjUWQK5b-aiLiKlr3bqfDmbhzcg
 
